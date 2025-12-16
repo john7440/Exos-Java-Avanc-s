9fe886d56ddc56bc583c7a84a1550397f4c84fea
Jonathan Maier <jonathan.maier@fms-ea.com>
2025-12-16 14:12:11 +0100

    modification de la class pour refléter le résultat attendu avec les 5 variables

--- src/fr/fms/ex/TestThread.java
+++ src/fr/fms/ex/TestThread.java
@@ -1,4 +1,26 @@
 package fr.fms.ex;
 
-public class TestThread {
+public class TestThread extends Thread {
+
+    public TestThread(String name) {
+        super(name);
+    }
+
+    public void run(){
+        for (int i = 0; i < 10; i++)
+            System.out.println(this.getName());
+    }
+
+    public static void main(String[] args){
+        TestThread t1 = new TestThread("1-");
+        TestThread t2 = new TestThread("2--");
+        TestThread t3 = new TestThread("3---");
+        TestThread t4 = new TestThread("4----");
+        TestThread t5 = new TestThread("5----");
+        t1.start();
+        t5.start();
+        t4.start();
+        t2.start();
+        t3.start();
+    } 
 }
